de9a464e3c55b3e9ce1f0ca278d48aeefb78bcdd
app2084 <isucon@app2084>
2017-11-25 13:24:45 +0900

    set -ex

--- profile.sh
+++ profile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 ~/local/python/bin/gprof2dot -f pstats --colour-nodes-by-selftime --show-samples /tmp/profile/* > profile.dot
 dot -Tpdf profile.dot -o profile.pdf
 rm /tmp/profile/*
